771c1e65565b28729fbae1a5568175fba3306b37
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-04-24 20:59:54 +0200

    fix(storybook): update vite configuration

--- packages/tools/storybook/.storybook/main.js
+++ packages/tools/storybook/.storybook/main.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   stories: [
     `../tmp/**/*.stories.@(tsx|mdx)`,
-    `../tmp/**/*.story.mdx`,
+    `../tmp/**/*.story.@(mdx)`,
     `../source/*.stories.@(mdx)`,
   ],
   features: {
@@ -17,21 +17,7 @@ module.exports = {
   core: {
     builder: 'storybook-builder-vite',
   },
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve('./'),
-      path.resolve('./packages'),
-    ];
-
-    config.devtool = false;
-
-    // Return the altered config
+  async viteFinal(config, { configType }) {
     return config;
   },
 };
