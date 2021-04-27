c4974a6b6dc5f132e77aa2d6bdc5e240c13dfaee
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-04-27 21:26:03 +0200

    chore(storybook): more cleanup

--- packages/tools/storybook/.storybook/main.js
+++ packages/tools/storybook/.storybook/main.js
@@ -1,7 +1,3 @@
-// const commonjs = require('@rollup/plugin-commonjs');
-// const resolve = require('@rollup/plugin-node-resolve').nodeResolve;
-// const optimizeLodashImports = require('@optimize-lodash/rollup-plugin').optimizeLodashImports;
-
 module.exports = {
   stories: [
     `../tmp/**/*.stories.@(tsx|mdx)`,
@@ -17,21 +13,6 @@ module.exports = {
   },
   async viteFinal(config, { configType }) {
     config.esbuild = { jsxInject: `import React from 'react'` };
-    /*config.optimizeDeps = { include: [
-      'core-js-pure',
-      'css-element-queries',
-      '@babel/runtime-corejs3',
-      'core-js-pure/features/object/get-own-property-symbols.js?commonjs-proxy',
-      'core-js-pure/features/object/get-own-property-symbols.js?commonjs-require',
-      'core-js-pure/features/object/get-own-property-symbols.js',
-      '@babel/runtime-corejs3/core-js/object/get-own-property-symbols.js',
-    ], auto: true };*/
-
-    /*if (configType === 'PRODUCTION') {
-      config.plugins.unshift(optimizeLodashImports());
-      config.plugins.unshift(commonjs());
-      config.plugins.unshift(resolve());
-    }*/
 
     return config;
   },
