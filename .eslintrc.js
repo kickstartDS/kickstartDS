2d6eb158364b77a17e7b42766877fa342215b6f4
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-05-04 14:03:54 +0200

    chore: fix linter
    by adding a lerna import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
+const path = require('path');
+const config = require('./packages/tools/eslint-config/index.js');
+
 module.exports = {
   extends: './packages/tools/eslint-config/index.js',
+  settings: {
+    'import/resolver': {
+      ...config.settings['import/resolver'],
+      'eslint-import-resolver-lerna': {
+        packages: path.resolve(__dirname, 'packages/components'),
+      },
+    },
+  },
 };
