507e49b8a4d05960c2c25f15d6edd9b7b5aee170
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-05-18 17:44:16 +0200

    feat: add radio- & checkbox-group component

--- packages/components/form/source/index.ts
+++ packages/components/form/source/index.ts
@@ -3,3 +3,5 @@ export * from './1-atoms/radio';
 export * from './1-atoms/select-field';
 export * from './1-atoms/text-area';
 export * from './1-atoms/text-field';
+export * from './2-molecules/checkbox-group';
+export * from './2-molecules/radio-group';
