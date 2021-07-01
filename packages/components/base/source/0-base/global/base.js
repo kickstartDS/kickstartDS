b719196359dc850a88683a9bc8801afe6517a603
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-07-01 13:12:25 +0200

    fix(ssr): access document only if available

--- packages/components/base/source/0-base/global/base.js
+++ packages/components/base/source/0-base/global/base.js
@@ -1,8 +1,11 @@
-import { domLoaded } from '@kickstartds/core/lib/core';
+import { domLoaded, inBrowser } from '@kickstartds/core/lib/core';
 import svgFixer from '../1-tools/js/svg-icon-baseurl';
 import '../2-generic/polyfills/polyfills';
 
-document.documentElement.classList.add('js');
+if (inBrowser) {
+  document.documentElement.classList.add('js');
+}
+
 domLoaded(() => {
   svgFixer();
 });
