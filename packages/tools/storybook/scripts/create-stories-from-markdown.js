50165ad8626a97ad9929a7eebf98a31492320152
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-07-04 12:43:25 +0200

    fix(documentation): add emoji support for CHANGELOG.mds

--- packages/tools/storybook/scripts/create-stories-from-markdown.js
+++ packages/tools/storybook/scripts/create-stories-from-markdown.js
@@ -2,16 +2,33 @@
 const fs = require('fs-extra');
 const glob = require('fast-glob');
 const { capitalCase } = require('change-case');
+const { toArray } = require('react-emoji-render');
 const template = require('../resources/templates/Markdown.story.mdx');
 const { root } = require('./utils');
+
+const parseEmojis = (value) => {
+  const emojisArray = toArray(value);
+
+  // toArray outputs React elements for emojis and strings for other
+  const newValue = emojisArray.reduce((previous, current) => {
+    if (typeof current === 'string') {
+      return previous + current;
+    }
+    return previous + current.props.children;
+  }, '');
+
+  return newValue;
+};
 
 const templateStory = (mod, name, mdContent) => {
   const options = {
     title: `${capitalCase(mod)}/${capitalCase(name)}`,
-    content: mdContent
-      .toString()
-      // add linebreak between `<a>`-tag and headline
-      .replace(/<\/a>(\r\n|\r|\n)#\s/g, '</a>\n\n# '),
+    content: parseEmojis(
+      mdContent
+        .toString()
+        // add linebreak between `<a>`-tag and headline
+        .replace(/<\/a>(\r\n|\r|\n)#\s/g, '</a>\n\n# ')
+    ),
   };
 
   return fs.outputFile(`tmp/${mod}/${name}.story.mdx`, template(options));
