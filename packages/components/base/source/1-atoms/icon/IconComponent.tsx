83dc4df9cbef1089a68303cb03628a3aaffe724d
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-11-08 09:12:41 +0100

    feat: add context and provider structure to `Icon` component

--- packages/components/base/source/1-atoms/icon/IconComponent.tsx
+++ packages/components/base/source/1-atoms/icon/IconComponent.tsx
@@ -1,3 +1,10 @@
+import { createContext, useContext } from 'react';
 import './icon.scss';
+import { Icon as IconComponent } from './PureIconComponent';
 
-export { Icon } from './PureIconComponent';
+export const IconContextDefault = IconComponent;
+export const IconContext = createContext(IconContextDefault);
+export const Icon: typeof IconContextDefault = (props) => {
+  const Component = useContext(IconContext);
+  return <Component {...props} />;
+};
