eeb3804d070623b7211855730c82236418987256
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-12-03 11:13:37 +0100

    style: eslint

--- packages/components/content/source/2-molecules/collapsible-box/lazyCollapsibleBox.js
+++ packages/components/content/source/2-molecules/collapsible-box/lazyCollapsibleBox.js
@@ -1,6 +1,6 @@
 import { define } from '@kickstartds/core/lib/core';
 import { identifier } from './CollapsibleBox.desc';
 
-export const lazyCollapsibleBox = () => import('./CollapsibleBox.js');
+export const lazyCollapsibleBox = () => import('./CollapsibleBox');
 
 define(identifier, lazyCollapsibleBox);
