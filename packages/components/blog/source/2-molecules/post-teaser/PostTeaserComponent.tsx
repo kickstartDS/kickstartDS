40580146163ed330b0e529ab5d245dbe0e7426df
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-02-23 16:13:23 +0100

    feat: refactor post-teaser

--- packages/components/blog/source/2-molecules/post-teaser/PostTeaserComponent.tsx
+++ packages/components/blog/source/2-molecules/post-teaser/PostTeaserComponent.tsx
@@ -1,5 +1,4 @@
 import {
-  FunctionComponent,
   ForwardRefRenderFunction,
   HTMLAttributes,
   forwardRef,
@@ -7,127 +6,65 @@ import {
   createContext,
   useContext,
 } from 'react';
-import { format } from 'date-fns';
 import classnames from 'classnames';
 import { renderFn, defaultRenderFn } from '@kickstartds/core/lib/core';
 import { TagLabelContainer } from '@kickstartds/base/lib/tag-label-container';
-import { Link } from '@kickstartds/base/lib/link';
-import { LinkButton } from '@kickstartds/base/lib/link-button';
+import { Teaser, TeaserRenderFunctions } from '@kickstartds/base/lib/teaser';
 import { Picture } from '@kickstartds/base/lib/picture';
-import {
-  RichText,
-  defaultRenderFn as richTextDefaultRenderFn,
-} from '@kickstartds/base/lib/rich-text';
+import { defaultRenderFn as richTextDefaultRenderFn } from '@kickstartds/base/lib/rich-text';
+import { PostMeta } from '../post-meta';
 import { PostTeaserProps } from './PostTeaserProps';
 import './post-teaser.scss';
 
-interface PostTeaserRenderFunctions {
-  renderTopic?: renderFn;
-  renderText?: renderFn;
-}
-
-const PostTeaserHeader: FunctionComponent<
-  PostTeaserProps & PostTeaserRenderFunctions
-> = ({ date, categories, index, link, renderTopic, title }) => (
-  <div className="c-post-teaser--header">
-    <div className="c-post-teaser__meta">
-      {date && (
-        <time
-          dateTime={format(new Date(date), 'yyy-MM-dd')}
-          className="c-post-teaser__date"
-        >
-          {format(new Date(date), 'dd.MM.yyyy')}
-        </time>
-      )}
-      {categories.length ? (
-        <TagLabelContainer
-          tagLabels={categories.map((category) => ({
-            ...category,
-            size: 's',
-          }))}
-        />
-      ) : (
-        ''
-      )}
-    </div>
-
-    <h2 className="c-post-teaser__headline" id={`news-header-${index}`}>
-      <Link href={link.href}>{renderTopic(title)}</Link>
-    </h2>
-  </div>
-);
-
 const PostTeaserComponent: ForwardRefRenderFunction<
   HTMLElement,
-  PostTeaserProps & PostTeaserRenderFunctions & HTMLAttributes<HTMLElement>
+  PostTeaserProps & TeaserRenderFunctions & HTMLAttributes<HTMLElement>
 > = (
   {
-    variant = 'image-first',
     image,
-    date,
     link,
     title,
     body,
     categories = [],
-    index,
     renderTopic = defaultRenderFn,
     renderText = richTextDefaultRenderFn,
+    meta,
     className,
     ...props
   },
   ref
 ) => (
   <article
-    className={classnames(
-      'c-post-teaser',
-      {
-        'c-post-teaser--image-first': variant === 'image-first',
-        'c-post-teaser--title-first': variant === 'title-first',
-      },
-      className
-    )}
+    className={classnames('c-post-teaser', className)}
     ref={ref}
     {...props}
   >
-    {variant === 'title-first' && (
-      <PostTeaserHeader
-        date={date}
-        categories={categories}
-        index={index}
-        link={link}
-        renderTopic={renderTopic}
-        title={title}
+    {categories && categories.length && (
+      <TagLabelContainer
+        tagLabels={categories.map((category) => ({
+          ...category,
+          size: 's',
+        }))}
       />
     )}
-    {image && (
-      <div className="c-post-teaser__image">
-        <Picture {...image} />
-      </div>
-    )}
-    <div className="c-post-teaser__body">
-      {variant === 'image-first' && (
-        <PostTeaserHeader
-          date={date}
-          categories={categories}
-          index={index}
-          link={link}
-          renderTopic={renderTopic}
-          title={title}
-        />
+    <Teaser
+      topic={title}
+      text={body}
+      link={{ ...link, hidden: true }}
+      renderTopic={renderTopic}
+      renderText={(text) => (
+        <>
+          {renderText(text)}
+          {meta && <PostMeta {...meta} />}
+        </>
       )}
-      {body && <RichText text={body} renderText={renderText} />}
-      <div className="c-post-teaser__more">
-        <LinkButton
-          id={`news-link-more-${index}`}
-          aria-labelledby={`news-link-more-${index} news-header-${index}`}
-          icon={{ icon: 'chevron-right' }}
-          iconAfter={true}
-          variant="clear"
-          size="medium"
-          {...link}
-        />
-      </div>
-    </div>
+    >
+      {image && (
+        <div className="c-post-teaser__image">
+          <Picture {...image} />
+        </div>
+      )}
+    </Teaser>
   </article>
 );
 
