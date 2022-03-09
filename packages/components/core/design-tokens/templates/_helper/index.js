0b5f0046e338ec3753148d21d57580237a83a0fe
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-03-09 18:00:55 +0100

    feat: calculate spacing scales

---
+++ packages/components/core/design-tokens/templates/_helper/index.js
@@ -0,0 +1,1 @@
+export const modularScale = (base, ratio) => (step) => ratio ** step * base;
--- packages/components/core/design-tokens/templates/spacing/index.js
+++ packages/components/core/design-tokens/templates/spacing/index.js
@@ -1,3 +1,5 @@
+const { modularScale } = require('../_helper');
+
 const rem = (number) => {
   const factor = 10 ** 4;
   return Math.round((number / 16) * factor) / factor;
@@ -5,19 +7,38 @@ const rem = (number) => {
 
 const attributes = { category: 'size' };
 
-module.exports = ({ spacing }) => ({
-  spacing: Object.entries(spacing).reduce((prev, [key, { base, bp }]) => {
-    prev[key] = {
-      _: {
-        value: rem(base),
-        attributes,
-        token: { category: 'Spacing', presenter: 'Spacing' },
-      },
-      bp: Object.entries(bp).reduce((prev2, [breakpoint, value]) => {
-        prev2[breakpoint] = { value: rem(value), attributes };
-        return prev2;
-      }, {}),
-    };
-    return prev;
-  }, {}),
-});
+const scales = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl'];
+const baseIndex = scales.indexOf('m');
+const breakpoints = ['s', 'm', 'xl'];
+
+module.exports = ({ spacing }) => {
+  const { base, 'scale-ratio': scaleRatio, 'bp-ratio': bpRatio } = spacing;
+  const scaleMs = modularScale(base, scaleRatio);
+  return {
+    spacing: Object.fromEntries(
+      scales.map((scale, index) => {
+        const value = scaleMs(index - baseIndex);
+        const breakpointMs = modularScale(value, bpRatio);
+        return [
+          scale,
+          {
+            _: {
+              value: rem(value),
+              attributes,
+              token: { category: 'Spacing', presenter: 'Spacing' },
+            },
+            bp: Object.fromEntries(
+              breakpoints.map((breakpoint, bpIndex) => [
+                breakpoint,
+                {
+                  value: rem(breakpointMs(bpIndex + 1)),
+                  attributes,
+                },
+              ])
+            ),
+          },
+        ];
+      })
+    ),
+  };
+};
