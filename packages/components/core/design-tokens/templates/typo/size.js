fbc8966c273db0d80d48686c417a39aff5d4cf8e
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-03-11 13:24:32 +0100

    feat: use modular scale to calculate font-size scales

--- packages/components/core/design-tokens/templates/typo/size.js
+++ packages/components/core/design-tokens/templates/typo/size.js
@@ -1,26 +1,44 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable guard-for-in */
+const { modularScale } = require('../_helper');
+
+const scales = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl'];
+const baseIndex = scales.indexOf('m');
+
 module.exports = ({ font }) =>
   Object.entries(font).reduce(
-    (prev, [family, { size, 'bp-factor': bpFactor }]) => {
-      prev['font-size'][family] ??= { 'bp-factor': {} };
-      prev['line-height'][family] ??= {};
-
-      for (const bp in bpFactor) {
-        prev['font-size'][family]['bp-factor'][bp] = { value: bpFactor[bp] };
-      }
-
-      for (const scale in size) {
-        prev['font-size'][family][`${scale}-base`] = {
-          value: `${size[scale][0] / 16}rem`,
-          attributes: { category: 'size' },
-          token: { category: `Font Sizes: ${family}`, presenter: 'FontSize' },
-        };
-
-        prev['line-height'][family][scale] = {
-          value: size[scale][1],
-        };
-      }
+    (
+      prev,
+      [
+        family,
+        {
+          'font-size': fontSize,
+          'line-height': lineHeight,
+          'scale-ratio': scaleRatio,
+          'bp-factor': bpFactor,
+        },
+      ]
+    ) => {
+      const fontSizeMs = modularScale(fontSize, scaleRatio);
+      prev['font-size'][family] = {
+        'bp-factor': Object.fromEntries(
+          Object.entries(bpFactor).map(([key, value]) => [key, { value }])
+        ),
+        ...Object.fromEntries(
+          scales.map((scale, index) => [
+            `${scale}-base`,
+            {
+              value: `${fontSizeMs(index - baseIndex) / 16}rem`,
+              attributes: { category: 'size' },
+              token: {
+                category: `Font Sizes: ${family}`,
+                presenter: 'FontSize',
+              },
+            },
+          ])
+        ),
+      };
+      prev['line-height'][family] = Object.fromEntries(
+        scales.map((scale) => [scale, { value: lineHeight }])
+      );
 
       return prev;
     },
