ff896bb12ba506947a98dc87afa029a48b36ed14
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-03-14 13:41:21 +0100

    fix: bring back font-family tokens

--- packages/components/core/design-tokens/templates/typo/font-family.js
+++ packages/components/core/design-tokens/templates/typo/font-family.js
@@ -2,10 +2,10 @@ module.exports = ({ font }) =>
   Object.entries(font).reduce(
     (prev, [key, { family }]) => ({
       ...prev,
-      // [key]: {
-      //   value: family,
-      //   token: { category: 'Font Families', presenter: 'FontFamily' },
-      // },
+      [key]: {
+        value: family,
+        token: { category: 'Font Families', presenter: 'FontFamily' },
+      },
     }),
     {}
   );
