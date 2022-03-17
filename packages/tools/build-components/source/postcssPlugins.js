36d19d6c5f0b389b1cd9271ea6f886207a3e698b
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-03-17 17:35:01 +0100

    build: sort container queries

--- packages/tools/build-components/source/postcssPlugins.js
+++ packages/tools/build-components/source/postcssPlugins.js
@@ -1,9 +1,15 @@
 const combineDuplicatedSelectors = require('postcss-combine-duplicated-selectors');
 const sortMediaQueries = require('postcss-sort-media-queries');
+const sortContainerQueries = require('postcss-sort-container-queries');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
-const dev = [sortMediaQueries(), combineDuplicatedSelectors(), autoprefixer()];
+const dev = [
+  sortMediaQueries(),
+  sortContainerQueries(),
+  combineDuplicatedSelectors(),
+  autoprefixer(),
+];
 const prod = [...dev, cssnano({ preset: 'default' })];
 
 module.exports = process.env.NODE_ENV === 'production' ? prod : dev;
