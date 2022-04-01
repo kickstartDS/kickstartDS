a38ee2009cac261cd69368a4ebb8c83f58367142
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-04-01 09:59:45 +0200

    fix: fix wrong paths in typescript definitions

--- packages/components/core/source/core/index.ts
+++ packages/components/core/source/core/index.ts
@@ -2,8 +2,6 @@ import './radio.js';
 
 export { domLoaded, inBrowser } from './domLoaded.js';
 export { render } from './html.js';
-export { uid } from './component/uid.js';
-export { define } from './component/define.js';
-export { Component } from './component/Component.js';
+export { Component, define, uid } from './component';
 export * from './renderFunctions';
 export { events, lazyChildrenLoaded } from './lazysizes.js';
