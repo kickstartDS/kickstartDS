f6353021de174afd33afe54329c7e429171ecf99
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-04-13 12:00:57 +0200

    fix(schema): usage of anyOf to extend enums

--- packages/tools/build-components/source/schemaDereference.js
+++ packages/tools/build-components/source/schemaDereference.js
@@ -1,9 +1,35 @@
 /* eslint-disable import/no-dynamic-require */
 const $RefParser = require('json-schema-ref-parser');
 const merge = require('json-schema-merge-allof');
+const traverse = require('json-schema-traverse');
+
 const { root } = require('./utils');
 
 const { schemaResolver } = require(`${root}/scripts/schemaResolver`);
+
+const mergeAnyOfEnums = (schema) => {
+  traverse(schema, {
+    cb: (subSchema) => {
+      if (
+        subSchema.type === 'string' &&
+        subSchema.enum &&
+        subSchema.anyOf &&
+        subSchema.anyOf.length === 2 &&
+        subSchema.anyOf.every((anyOf) => anyOf.type === 'string' && anyOf.enum)
+      ) {
+        subSchema.enum = subSchema.anyOf.reduce((enumValues, anyOf) => {
+          anyOf.enum.forEach((value) => {
+            if (!enumValues.includes(value)) enumValues.push(value);
+          });
+
+          return enumValues;
+        }, []);
+
+        delete subSchema.anyOf;
+      }
+    },
+  });
+};
 
 const schemaLoader = async (refParser, schemaPath) =>
   refParser.dereference(schemaPath, {
@@ -17,6 +43,8 @@ const schemaLoader = async (refParser, schemaPath) =>
 
 const dereference = async (schemaPath) => {
   const schema = await schemaLoader(new $RefParser(), schemaPath);
+  mergeAnyOfEnums(schema);
+
   return merge(schema, { ignoreAdditionalProperties: true });
 };
 
