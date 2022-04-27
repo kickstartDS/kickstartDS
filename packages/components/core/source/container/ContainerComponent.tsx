0225405b9396d888c53a256196f24bff0fc4d2d8
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-04-27 14:38:41 +0200

    refactor: unify container component exports

--- packages/components/core/source/container/ContainerComponent.tsx
+++ packages/components/core/source/container/ContainerComponent.tsx
@@ -1,27 +1,11 @@
-import {
-  ForwardRefRenderFunction,
-  HTMLAttributes,
-  forwardRef,
-  createElement,
-  createContext,
-  useContext,
-  ForwardRefExoticComponent,
-  ForwardedRef,
-  ElementRef,
-  ComponentPropsWithoutRef,
-  PropsWithoutRef,
-  RefAttributes,
-  Context,
-} from 'react';
+import { type ForwardRefRenderFunction, type HTMLAttributes } from 'react';
 import classnames from 'classnames';
-import './container.scss';
-import './Container.js';
 
-type ContainerProps = HTMLAttributes<HTMLDivElement> & {
+export type ContainerProps = HTMLAttributes<HTMLDivElement> & {
   name?: string;
 };
 
-const ContainerComponent: ForwardRefRenderFunction<
+export const ContainerComponent: ForwardRefRenderFunction<
   HTMLDivElement,
   ContainerProps
 > = ({ name, className, ...props }, ref) => (
@@ -36,36 +20,3 @@ const ContainerComponent: ForwardRefRenderFunction<
     {...props}
   />
 );
-
-export const ContainerContextDefault = forwardRef(ContainerComponent);
-export const ContainerContext = createContext(ContainerContextDefault);
-export const Container: typeof ContainerContextDefault = forwardRef(
-  (props, ref) => createElement(useContext(ContainerContext), { ...props, ref })
-);
-
-export const withContainer = <
-  C extends ForwardRefExoticComponent<any>,
-  T = ElementRef<C>,
-  P = ComponentPropsWithoutRef<C>
->(
-  name: string,
-  context: Context<C>
-): ForwardRefExoticComponent<
-  PropsWithoutRef<P & { container?: ContainerProps }> & RefAttributes<T>
-> =>
-  forwardRef(
-    (
-      {
-        container = {},
-        ...props
-      }: P & {
-        container?: ContainerProps;
-      },
-      ref: ForwardedRef<T>
-    ) =>
-      createElement(
-        Container,
-        { ...container, name },
-        createElement(useContext(context), { ...props, ref })
-      )
-  );
