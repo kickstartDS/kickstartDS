cdf8e3dbbcc50f57a118ad2639a4a062254faba2
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-06-21 11:33:44 +0200

    build: move style-dictionary config in its own module

--- packages/components/core/scripts/build-tokens.js
+++ packages/components/core/scripts/build-tokens.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const StyleDictionary = require('style-dictionary');
 const merge = require('lodash/merge');
-const { config, createTokens } = require('../design-tokens');
+const { config, createTokens } = require('@kickstartds/style-dictionary');
 
 StyleDictionary.extend(config)
   .extend({
