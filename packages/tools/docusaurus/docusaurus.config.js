13511bcb62759bd723a9a236e469c6e9b0aac08f
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-08-05 14:06:22 +0200

    feat(docs): add basic docusaurus docs

---
+++ packages/tools/docusaurus/docusaurus.config.js
@@ -0,0 +1,146 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const theme = require('shiki/themes/nord.json');
+const { remarkCodeHike } = require('@code-hike/mdx');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'kickstartDS docs',
+  tagline: '... easy like squeeeeezing a lemon',
+  url: 'https://www.kickstartDS.com',
+  baseUrl: '/docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'kickstartDS', // Usually your GitHub org/user name.
+  projectName: 'kickstartDS', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          // path: '../../components/core/docs',
+          beforeDefaultRemarkPlugins: [[remarkCodeHike, { theme }]],
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/kickstartDS/kickstartDS/tree/main/packages/components/core/docs/',
+        },
+        blog: false,
+        theme: {
+          customCss: [
+            require.resolve('@kickstartds/core/lib/design-tokens/tokens.css'),
+            require.resolve('@kickstartds/base/lib/global/base.css'),
+            require.resolve('@code-hike/mdx/styles.css'),
+            require.resolve('./src/css/ks-tokens.css'),
+            require.resolve('./src/css/mapping.css'),
+            require.resolve('./src/css/custom.css'),
+          ],
+        },
+      }),
+    ],
+  ],
+
+  themes: ['mdx-v2'],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'kickstartDS Logo',
+          src: 'img/logo.svg',
+          srcDark: 'img/logo-light.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/kickstartDS/kickstartDS',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting started',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/mwKzD5gejY',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/kickstartds',
+              },
+              {
+                label: 'Stackshare',
+                href: 'https://stackshare.io/kickstartds/kickstartds',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'https://www.kickstartds.com/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/kickstartDS/kickstartDS',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} kickstartDS, ruhmesmeile GmbH. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      announcementBar: {
+        id: 'beta_release',
+        content:
+          "We're currently approaching our first major open source release (2.0.0), read more in this <a target='_blank' rel='noopener noreferrer' href='#'>blog post</a>",
+        backgroundColor: 'var(--ks-color-primary)',
+        textColor: 'var(--ks-color-primary-inverted)',
+        isCloseable: false,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ packages/tools/docusaurus/src/pages/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+// import { Visual } from '@kickstartds/content/lib/visual';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <>
+      {/* <Visual
+        className="c-visual--welcome"
+        box={{
+          background: 'transparent',
+          enabled: true,
+          headline: {
+            level: 'h1',
+            styleAs: 'h1',
+            content: 'Welcome 👋',
+            subheadline: 'Get started with the kickstartDS Design System!',
+          },
+          horizontal: 'center',
+          indent: false,
+          link: {
+            enabled: false,
+            fillAnimation: false,
+            href: 'https://example.com',
+            iconAfter: true,
+            iconAnimation: false,
+            iconBefore: false,
+            icon: {
+              icon: 'chevron-right',
+            },
+            label: 'Show ',
+            newTab: false,
+            size: 'medium',
+            variant: 'solid',
+          },
+          text: "Explore our frontend first framework, and all of its comprehensive components backing our landing page, our Wordpress blog and the glossary. Take a deeper look at our design and component tokens, interact with the Storybook controls, or preview some sample pages we've assembled.",
+          vertical: 'top',
+        }}
+        height="small"
+        media={{
+          image: {
+            indent: 'none',
+            srcDesktop: 'img/banner/welcome-centered.svg',
+            srcTablet: 'img/banner/welcome-centered.svg',
+            srcMobile: 'img/banner/welcome-centered-m.svg',
+          },
+          mode: 'image',
+        }}
+      /> */}
+
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/migrating/upgrade-2.0.0"
+            >
+              Learn how to migrate to 2.0.0 - 5min ⏱️
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
