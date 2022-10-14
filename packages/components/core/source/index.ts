33a417caae4ae7c3cc656f9cbe376f3b47f62a1f
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-10-14 16:06:30 +0200

    build: remove package's single entrypoint

--- packages/components/core/source/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './container/ContainerComponent';
-export * from './container/index';
-export * from './core/component/index';
-export * from './core/index';
-export * from './storybook/index';
-export * from './utils/index';
