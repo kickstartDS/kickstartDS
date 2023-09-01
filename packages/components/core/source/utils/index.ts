da7fb11ce3ffd7f55eb375b0e4cf7bbc88944983
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2023-09-01 12:32:03 +0200

    feat: add debounce & throttle utility functions

--- packages/components/core/source/utils/index.ts
+++ packages/components/core/source/utils/index.ts
@@ -1,3 +1,5 @@
 export { slideDown, slideUp, slideToggle } from './slide.js';
 export { windowEvents } from './window.js';
 export { createProvider } from './createProvider';
+export { debounce } from './debounce.js';
+export { throttle } from './throttle.js';
