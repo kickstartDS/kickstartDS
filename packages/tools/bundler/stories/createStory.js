517b807e627bb0d823b10f3a9b9e48f3097409e3
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2023-09-29 15:30:18 +0200

    build: fix json schema types in generated stories

--- packages/tools/bundler/stories/createStory.js
+++ packages/tools/bundler/stories/createStory.js
@@ -6,25 +6,27 @@ const template = ({
   componentName,
   componentPascalcased,
   schema,
-  loadComponentTokens,
+  hasComponentTokens,
 }) => `
 import type { Meta, StoryObj } from '@storybook/react';
+import type { JSONSchema7 } from "json-schema";
 import { getArgsShared } from "@kickstartds/core/lib/storybook";
 import { ${componentPascalcased} } from './index.js';
 ${
-  loadComponentTokens
+  hasComponentTokens
     ? `import cssprops from './${componentName}-tokens.json';`
     : ''
 }
 
-const schema = ${JSON.stringify(schema, null, 2)};
+const schema: JSONSchema7 = ${JSON.stringify(schema, null, 2)};
 
 const meta: Meta<typeof ${componentPascalcased}> = {
   title: '${capitalCase(moduleDir)}/${capitalCase(schema.title)}',
   component: ${componentPascalcased},
   excludeStories: ['Template'],
   parameters: {
-    ${loadComponentTokens ? 'cssprops,' : ''}
+    ${hasComponentTokens ? 'cssprops,' : ''}
+    jsonschema: schema,
   },
   ...getArgsShared(schema),
 };
@@ -41,18 +43,16 @@ const createStory = (schema, dest) => {
     `${dest}/${componentName}-tokens.json`
   );
   if (type === 'schema' && schema.properties) {
-    return Promise.all([
-      fs.outputFile(
-        `${dest}/${componentName}.stories.ts`,
-        template({
-          moduleDir,
-          componentName,
-          componentPascalcased: pascalCase(componentName),
-          schema,
-          hasComponentTokens,
-        })
-      ),
-    ]);
+    return fs.outputFile(
+      `${dest}/${componentName}.stories.ts`,
+      template({
+        moduleDir,
+        componentName,
+        componentPascalcased: pascalCase(componentName),
+        schema,
+        hasComponentTokens,
+      })
+    );
   }
 };
 
